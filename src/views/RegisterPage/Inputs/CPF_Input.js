97433f1e58e710a0e7aa532cad2e181775e10905
Matheus Machado <40525348+notsanchez@users.noreply.github.com>
2022-03-15 21:45:20 -0300

    Update CPF_Input.js

--- src/views/RegisterPage/Inputs/CPF_Input.js
+++ src/views/RegisterPage/Inputs/CPF_Input.js
@@ -3,8 +3,4 @@ const CPFInput = (props) => (
   <InputMask mask="999.999.999-99" value={props.value} onChange={props.onChange} className='default-input input-dark' name='id' placeholder='CPF'/>
 );
 
-const TEL_Input = (props) => (
-  <InputMask mask="+55 999999999" value={props.value} onChange={props.onChange} className='default-input input-dark' name='phone' placeholder='Telefone' />
-);
-
 export default CPFInput;
